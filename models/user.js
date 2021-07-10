2e85d0bd66e92f3849a4b0d3fd346f5f8c4e7392
Isaac Rudnick <isaac.t.rudnick@gmail.com>
2021-07-09 20:20:57 -0400

    Changed ECphoneNumbers to array, minor other changes.

--- models/user.js
+++ models/user.js
@@ -4,23 +4,22 @@ const Schema = mongoose.Schema;
 //User will later add in other data or information
 const userSchema = new Schema({
     // user email
-    email: { type: String, required: true },
+    email: { type: String },
     // user's phone number
-    phoneNumber: { type: String, required: true },
+    phoneNumber: { type: String },
     // high threshold for user
-    highValue: { type: String, required: true },
-        // low threshhold for user
-    lowValue: { type: String, required: true },
+    highValue: { type: Number },
+    // low threshhold for user
+    lowValue: { type: String },
     // which numbers to text if user doesn't respond (family, friends, etc.) Emergency Contact Phone Number
     // Key is name and value is phone number. For example, 
-    // { "John": "+1 123-456-7890", "Greg": "+1 222-333-4444" }
-    ECphoneNumbers: { type: Object, required: true },
+    ECphoneNumbers: { type: Array },
     // how long out-of-range readings must continue before ECs are texted with alert
-    textECsAfter: { type: String, required: true },
+    textECsAfter: { type: String },
     // How long to wait before checking on user again. Minutes
-    userOkSnooze: {type: Number, required: true},
+    userOkSnooze: { type: Number },
     //user data source (NS such as example-bg.herokuapp.com/)
-    userDataSource: { type: String, required: true }
+    userDataSource: { type: String }
 },
     { timestamps: true });
 
