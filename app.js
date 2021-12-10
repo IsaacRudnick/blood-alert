6ab4993ef465b1f4399805ec0677c57351551f66
Isaac R <isaac.t.rudnick@gmail.com>
2021-12-09 20:17:28 -0500

    Remove Morgan Import

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser')
-const logger = require('morgan');
+// const logger = require('morgan');
 const mongoose = require('mongoose');
 const csrf = require('csurf');
 
